90a13f02925b253314d7d38835e67e73476cd0d1
dmagdaleno <diegommagdaleno@gmail.com>
2019-06-25 14:42:26 -0300

    initial commit

---
+++ src/main/kotlin/com/dmagdaleno/kalg/KalgApplication.kt
@@ -0,0 +1,11 @@
+package com.dmagdaleno.kalg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KalgApplication
+
+fun main(args: Array<String>) {
+	runApplication<KalgApplication>(*args)
+}
