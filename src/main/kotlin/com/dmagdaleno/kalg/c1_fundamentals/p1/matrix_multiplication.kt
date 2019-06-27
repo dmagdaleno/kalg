73f17f46910ed10bc449fb75cae2332c12a5dda1
dmagdaleno <diegommagdaleno@gmail.com>
2019-06-27 10:27:33 -0300

    import matrix extension function

--- src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p1/matrix_multiplication.kt
+++ src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p1/matrix_multiplication.kt
@@ -1,13 +1,9 @@
 package com.dmagdaleno.kalg.c1_fundamentals.p1
 
+import com.dmagdaleno.kalg.extensions.print
+
 fun main(args: Array<String>) {
     val array = arrayOf(1, 2, 3)
     val a = arrayOf(array.copyOf(), array.copyOf(), array.copyOf())
     a.print()
-}
-
-fun <T> Array<Array<T>>.print() {
-    println(this.joinToString(separator = "\n") {
-        array ->  array.joinToString(prefix = "| ", postfix = " |")
-    })
 }
