a097fa35311bfead9be348f16b4607838136e723
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-07-21 23:39:34 -0300

    add StdDraw dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.googlecode.princeton-java-introduction:stdlib:1.0.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
