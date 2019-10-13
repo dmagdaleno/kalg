e82a32f2be3c24af2e59c61605798ab9aaf546c0
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-10-12 21:25:13 -0300

    add check to the union method and decrement count

--- src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p5/QuickFind.kt
+++ src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p5/QuickFind.kt
@@ -5,14 +5,20 @@ class QuickFind(private val size: Int): UnionFindTemplate(size) {
     override fun find(p: Int): Int = id[p]
 
     override fun union(p: Int, q: Int) {
-        val pid = id[p]
-        val qid = id[q]
+        val pid = find(p)
+        val qid = find(q)
+
+        if(pid == qid) {
+            return
+        }
 
         for(i in 0 until size) {
             if(id[i] == pid) {
                 id[i] = qid
             }
         }
+
+        count--
     }
 
 }
--- src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p5/UnionFindTemplate.kt
+++ src/main/kotlin/com/dmagdaleno/kalg/c1_fundamentals/p5/UnionFindTemplate.kt
@@ -3,10 +3,11 @@ package com.dmagdaleno.kalg.c1_fundamentals.p5
 abstract class UnionFindTemplate(private val size: Int): UnionFind {
 
     protected val id: IntArray = IntArray(size){it}
+    protected var count = size
 
     override fun connected(p: Int, q: Int): Boolean = find(p) == find(q)
 
-    override fun count(): Int = size
+    override fun count(): Int = count
 
     fun print() {
         println(id.joinToString(prefix = "[", postfix = "]"))
